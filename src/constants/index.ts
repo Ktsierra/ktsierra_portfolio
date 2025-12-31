de43a5b10a4a5f40238ba22ece63a051dffdabfe
Ktsierra <josejaviersierra1998@gmail.com>
2025-12-31 18:05:46 -0500

    fixed jja dates

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -26,7 +26,7 @@ import {
   firebase,
   kubernetes,
   testFidel,
-  testRamon
+  testRamon,
 } from "../assets";
 
 export const navLinks = [
@@ -67,78 +67,78 @@ const technologies = [
   {
     name: "HTML 5",
     icon: html,
-    link: ''
+    link: "",
   },
   {
     name: "CSS 3",
     icon: css,
-    link: ''
+    link: "",
   },
   {
     name: "JavaScript",
     icon: javascript,
-    link: ''
+    link: "",
   },
   {
     name: "TypeScript",
     icon: typescript,
-    link: ''
+    link: "",
   },
   {
     name: "React JS",
     icon: reactjs,
-    link: ''
+    link: "",
   },
   {
     name: "Expo",
     icon: expo,
-    link: ''
+    link: "",
   },
   {
-    name: 'AWS',
+    name: "AWS",
     icon: aws,
-    link: ''
+    link: "",
   },
   {
     name: "Firebase",
     icon: firebase,
-    link: ''
+    link: "",
   },
   {
     name: "Tailwind CSS",
     icon: tailwind,
-    link: ''
+    link: "",
   },
   {
     name: "Node JS",
     icon: nodejs,
-    link: ''
+    link: "",
   },
   {
     name: "Python",
     icon: python,
-    link: ''
+    link: "",
   },
   {
     name: "Three JS",
     icon: threejs,
-    link: ''
+    link: "",
   },
   {
     name: "git",
     icon: git,
-    link: ''
+    link: "",
   },
   {
     name: "docker",
     icon: docker,
-    link: ''
+    link: "",
   },
   {
     name: "Kubernetes",
     icon: kubernetes,
-    link: ''
-  }
+    link: "",
+  },
 ];
 
 const experiences = [
@@ -150,7 +150,7 @@ const experiences = [
     date: "September 2021 - August 2023",
     points: [
       "Designed and implemented Python applications that analyze mobility and balance patterns in senior populations, enhancing research efficiency and data accuracy.",
-      "Contributed to machine learning models that forecast gait speed metrics in elderly populations, improving health outcome predictions through advanced algorithms and cross-team collaboration."
+      "Contributed to machine learning models that forecast gait speed metrics in elderly populations, improving health outcome predictions through advanced algorithms and cross-team collaboration.",
     ],
   },
 
@@ -172,20 +172,20 @@ const experiences = [
     company_name: "JJA Aviation",
     icon: jjaaviation,
     iconBg: "#ffffff",
-    date: "Jan 2021 - Feb 2022",
+    date: "September 2023 -  Ongoing",
     points: [
       "Developed full-stack applications including a product lifecycle management system (TypeScript, React, React Native, AWS Infrastructure), and internal web services featuring network documentation (Docusaurus, Firebase) and document indexing (Express, PostgreSQL, Docker)",
       "Designed and maintained comprehensive network infrastructure utilizing Ubiquiti ecosystem (EdgeSwitches, EdgeRouters, UniFi APs) and Synology NAS systems, while implementing cloud-based 3CX phone system in AWS to enhance communication capabilities",
       "Orchestrated critical containerized services through Docker deployment, integrating osTicket for support management, Grafana monitoring stack (Prometheus/Telegraf/InfluxDB), MeshCentral for remote management, and CUPS printing solutions",
-      "Established comprehensive security and management infrastructure including Microsoft Entra ID with Intune MDM for device management, and implemented Squid proxy with ELK stack for network monitoring, security analysis, and access control"
+      "Established comprehensive security and management infrastructure including Microsoft Entra ID with Intune MDM for device management, and implemented Squid proxy with ELK stack for network monitoring, security analysis, and access control",
     ],
-  }
+  },
 ];
 
 const testimonials = [
   {
     testimonial:
-        "He demonstrated mastery of Python programming while implementing and validating an Artificial Neural Networt and successfully developed a wireless system for human gait analysis incorporating signal processing techniques.",
+      "He demonstrated mastery of Python programming while implementing and validating an Artificial Neural Networt and successfully developed a wireless system for human gait analysis incorporating signal processing techniques.",
     name: "Fidel Montero",
     linkedIn: "https://www.linkedin.com/in/fidel-hernandez-25b7b370/",
     designation: "Vice Dean - R&PS, Faculty",
@@ -193,10 +193,9 @@ const testimonials = [
     image: testFidel,
   },
   {
-    testimonial:
-        "FILL",
+    testimonial: "FILL",
     name: "Ramon Mompeller",
-    linkedIn: 'https://www.linkedin.com/in/ramon-mompeller-64097a16a/',
+    linkedIn: "https://www.linkedin.com/in/ramon-mompeller-64097a16a/",
     designation: "CEO",
     company: "JJA Aviation",
     image: testRamon,
@@ -207,7 +206,7 @@ const projects = [
   {
     name: "SkyVault",
     description:
-        "Multiplatform mobile and web application that enables JJA Aviation to manage maintenance records and securely share aircraft component lifecycle information and images with clients and between departments.",
+      "Multiplatform mobile and web application that enables JJA Aviation to manage maintenance records and securely share aircraft component lifecycle information and images with clients and between departments.",
     tags: [
       {
         name: "react-native",
@@ -225,12 +224,12 @@ const projects = [
     image: skyvault,
     source_code_link: undefined,
     app_link: undefined,
-    app_icon: undefined
+    app_icon: undefined,
   },
   {
     name: "JJA Aviation Docs",
     description:
-        "Web application that documents JJA Aviation's technical infrastructure and maintenance systems configurations, providing authorized personnel with secure access to essential operational information.",
+      "Web application that documents JJA Aviation's technical infrastructure and maintenance systems configurations, providing authorized personnel with secure access to essential operational information.",
     tags: [
       {
         name: "react",
@@ -248,12 +247,12 @@ const projects = [
     image: jjadocs,
     source_code_link: undefined,
     app_link: undefined,
-    app_icon: undefined
+    app_icon: undefined,
   },
   {
     name: "NovaEdge Tech Website",
     description:
-        "Simple, effective company landing page focused on connecting with potential clients. Features a clean design with easy-to-use contact forms and clear information about my services. Currently refining mobile responsiveness and performance.",
+      "Simple, effective company landing page focused on connecting with potential clients. Features a clean design with easy-to-use contact forms and clear information about my services. Currently refining mobile responsiveness and performance.",
     tags: [
       {
         name: "three.js",
@@ -271,9 +270,9 @@ const projects = [
     image: novaedge,
     source_code_link: "https://github.com/Ktsierra/novaedgetech_web",
     app_link: "https://novaedgetechnology.com",
-    app_icon: novaedgeIcon
+    app_icon: novaedgeIcon,
   },
-/*   {
+  /*   {
     name: "Verde",
     description:
         "Multiplatform application designed to help home gardeners users, still in development",
